07beb72c3aed06988eaea825b8f0cd7f5deeab59
tomkluas <1175704775@qq.com>
2015-09-21 23:02:45 +0800

    bugfix

---
+++ wtgou.cn/Libraries/Nop.Data/Mapping/Customers/CustomerInviteCodeMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Domain.Configuration;
+
+namespace Nop.Data.Mapping.Customers
+{
+    public class CustomerInviteCodeMap : NopEntityTypeConfiguration<CustomerInviteCode>
+    {
+        public CustomerInviteCodeMap()
+        {
+            this.ToTable("CustomerInviteCode");
+            this.HasKey(c => c.Id);
+            this.Property(c => c.CustomerId);
+            this.Property(c => c.Code);
+            this.Property(c => c.IsUsed);
+        }
+    }
+}
