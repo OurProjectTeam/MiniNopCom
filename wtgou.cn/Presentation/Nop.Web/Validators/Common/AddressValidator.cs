26c4184bda1be3ed42c794e6705a73cdaee396b5
tomkluas <951046830@qq.com>
2015-10-13 21:35:00 +0800

    主题修改

--- wtgou.cn/Presentation/Nop.Web/Validators/Common/AddressValidator.cs
+++ wtgou.cn/Presentation/Nop.Web/Validators/Common/AddressValidator.cs
@@ -17,9 +17,9 @@ namespace Nop.Web.Validators.Common
             RuleFor(x => x.FirstName)
                 .NotEmpty()
                 .WithMessage(localizationService.GetResource("Address.Fields.FirstName.Required"));
-            RuleFor(x => x.LastName)
-                .NotEmpty()
-                .WithMessage(localizationService.GetResource("Address.Fields.LastName.Required"));
+            //RuleFor(x => x.LastName)
+            //    .NotEmpty()
+            //    .WithMessage(localizationService.GetResource("Address.Fields.LastName.Required"));
             RuleFor(x => x.Email)
                 .NotEmpty()
                 .WithMessage(localizationService.GetResource("Address.Fields.Email.Required"));
